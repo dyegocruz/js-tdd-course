86c397deee805e291fc2d72b50d6ad88dbf8cb78
Dyego Cruz <dyegocruz@gmail.com>
2018-04-05 17:18:36 -0300

    Starting with tests.

---
+++ tests/main.spec.js
@@ -0,0 +1,17 @@
+describe('Main', function() {
+  describe('Method A', function() {
+    context('Case 1', function() {
+      it('should happen blablabla', function() {
+        // espera que aconteça
+      });
+    });
+
+    context('Case 2', function() {
+
+    });
+  });
+
+  describe('Method B', function() {
+
+  });
+});
