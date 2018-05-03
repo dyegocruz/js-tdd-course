1e5a24e5f1eb34515277cf8629aac23e5af7e5b0
Dyego Cruz <dyegocruz@gmail.com>
2018-05-03 15:06:38 -0300

    Init test commands.

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,17 +1,35 @@
+var expect = require('chai').expect;
+
 describe('Main', function() {
-  describe('Method A', function() {
-    context('Case 1', function() {
-      it('should happen blablabla', function() {
-        // espera que aconteça
-      });
-    });
+  var arr = [];
 
-    context('Case 2', function() {
-
-    });
+  // roda todas as vezes, antes de CADA bloco
+  beforeEach(function() {
+    arr = [1, 2, 3];
   });
 
-  describe('Method B', function() {
-
+  // testar tipos ou se existe (smoke test)
+  it('should be an array', function() {
+    expect(arr).to.be.a('array');
   });
+
+  it('should have a size of 4 when push another value to the array', function() {
+    arr.push(4);
+    expect(arr).to.have.lengthOf(4);
+  });
+
+  it('should have a size of 2 when pop a value from the array', function() {
+    arr.pop();
+    expect(arr).to.not.include(3);
+  });
+
+  it('should return true when pop 3 from the array', function() {
+    expect(arr.pop() === 3).to.be.true;
+  });
+
+  it('should remove the value 3 when use pop in the array', function() {
+    arr.pop();
+    expect(arr).to.have.lengthOf(2);
+  });
+
 });
